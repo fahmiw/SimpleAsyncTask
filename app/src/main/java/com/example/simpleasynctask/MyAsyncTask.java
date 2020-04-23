766e25449c3e170c5a3ce5b9e59a95c91ed1caae
fahmiw <fahmi.widianto.tif18@polban.ac.id>
2020-04-23 12:16:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/simpleasynctask/MyAsyncTask.java
@@ -0,0 +1,11 @@
+package com.example.simpleasynctask;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+
+public class MyAsyncTask extends AsyncTask<String, Integer, Bitmap> {
+    @Override
+    protected Bitmap doInBackground(String... strings) {
+        return null;
+    }
+}
